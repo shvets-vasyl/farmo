3f007129d1c39fe0bde57c70fde5d8fa26e008e2
dev-vasyl <vasyl@thefirstthelast.agency>
2024-10-22 11:09:04 +0300

    test

--- composables/useUserData.ts
+++ composables/useUserData.ts
@@ -9,15 +9,17 @@ import { paths } from "@/utils/api/paths";
 import { getPhoto } from "@/utils/getPhoto"
 
 export const useUserData = () => {
-  let userId = localStorage.getItem("userId");
+  // let userId = localStorage.getItem("userId");
 
-  if (!userId && window.Telegram) {
-    const id = window.Telegram.WebApp.initDataUnsafe.user.id
-    // const id = "696325148";
+  // if (!userId && window.Telegram) {
+  //   const id = window.Telegram.WebApp.initDataUnsafe.user.id
+  //   // const id = "696325148";
 
-    localStorage.setItem("userId", id);
-    userId = id;
-  }
+  //   localStorage.setItem("userId", id);
+  //   userId = id;
+  // }
+
+	let userId = "696325148"
 
   const fetchUserProfile = async () => {
     const data = await $fetch<UserProfileInterface>("/api/get-data", {
